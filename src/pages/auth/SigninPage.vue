a46019eac600b6441fb2150f8c6b07420f54af9d
Whatdoyumin <lym4963@naver.com>
2025-07-22 00:44:29 +0900

    ✨ feat: 로그인 여부 pinia로 관리

--- src/pages/auth/SigninPage.vue
+++ src/pages/auth/SigninPage.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>로그인 페이지</div>
+  <div class="p-8 flex flex-col gap-4">
+    <h1 class="text-xl font-bold">로그인 페이지</h1>
+
+    <BaseButton v-if="!authStore.isLoggedIn" variant="primary" class="w-fit" @click="handleLogin">
+      로그인하기
+    </BaseButton>
+
+    <BaseButton v-else variant="primary" class="w-fit" @click="handleLogout">
+      로그아웃하기
+    </BaseButton>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import BaseButton from '@/components/common/BaseButton.vue'
+import { useAuthStore } from '@/stores/auth'
 
-<style scoped></style>
+const authStore = useAuthStore()
+
+function handleLogin() {
+  authStore.login()
+}
+
+function handleLogout() {
+  authStore.logout()
+}
+</script>
