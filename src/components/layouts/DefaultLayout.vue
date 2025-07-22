53ba8c92c10c5124c11eb1e1add5ffb5d2fcffb7
Whatdoyumin <lym4963@naver.com>
2025-07-22 14:11:02 +0900

    💄 style: 전체 레이아웃 x, y 간격 커스텀 변수 설정

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -1,16 +1,17 @@
 <!-- 기본 레이아웃 컴포넌트 -->
-<script setup>
-import Header from './HeaderLayout.vue'
-import Footer from './FooterLayout.vue'
-</script>
 
 <template>
   <div class="flex flex-col min-h-screen">
     <Header />
     <NavBar />
-    <main class="flex-1 my-5 px-16">
+    <main class="flex-1 my-layout-y px-layout-x">
       <slot />
     </main>
     <Footer />
   </div>
 </template>
+
+<script setup>
+import Header from './HeaderLayout.vue'
+import Footer from './FooterLayout.vue'
+</script>
