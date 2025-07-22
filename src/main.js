29b4c5d10c90200c658cc20e996ca1aa5b817805
Whatdoyumin <lym4963@naver.com>
2025-07-22 17:25:54 +0900

    🌱 chore: vueAwesomePage와 kakaoMap 전역 사용 설정

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import './assets/index.css'
+import 'vue-awesome-paginate/dist/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueAwesomePaginate } from 'vue-awesome-paginate'
 
 import App from './App.vue'
 import router from './router'
+import { useKakao } from 'vue3-kakao-maps/@utils'
 
 const app = createApp(App)
+const kakao_map_key = import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY
+useKakao(kakao_map_key, ['services'])
 
+app.use(VueAwesomePaginate)
 app.use(createPinia())
 app.use(router)
 
