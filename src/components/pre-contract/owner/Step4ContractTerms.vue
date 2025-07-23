8b79ba04084c9be44f7e13a645fdf7038e34bfa0
Whatdoyumin <lym4963@naver.com>
2025-07-23 11:08:03 +0900

    ✨ feat: 4단계 헤더 구현

--- src/components/pre-contract/owner/Step4ContractTerms.vue
+++ src/components/pre-contract/owner/Step4ContractTerms.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>임대인 계약 전 사전 조사 step 4</div>
+  <div class="flex flex-col items-center justify-center gap-8">
+    <div class="flex flex-col items-center justify-center gap-2">
+      <h1 class="text-gray-warm-700 font-bold text-xl">계약 기본 정보</h1>
+      <p class="text-gray-500">계약의 기본 조건을 설정해주세요</p>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
