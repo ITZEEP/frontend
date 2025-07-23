49c4fbda1acff1d9c319e590646fa5e0413a26a4
MeongW <easymiracle@skuniv.ac.kr>
2025-07-23 14:43:50 +0900

    💄 style: 아이콘 추가

---
+++ src/components/icons/IconCheck.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="none"
+    class="icon"
+  >
+    <path
+      d="M16.6667 5L7.5 14.1667L3.33333 10"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/components/icons/IconChevronLeft.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="none"
+    class="icon"
+  >
+    <path
+      d="M12.5 15L7.5 10L12.5 5"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
