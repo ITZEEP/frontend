597a7c91537f1de259bcae049d49f160fd0cbf9f
MeongW <easymiracle@skuniv.ac.kr>
2025-07-23 13:04:42 +0900

    💄 style: > 모양 아이콘 추가

---
+++ src/components/icons/IconChevronRight.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 8 14"
+    fill="none"
+    class="icon"
+  >
+    <path
+      d="M1 1L7 7L1 13"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
