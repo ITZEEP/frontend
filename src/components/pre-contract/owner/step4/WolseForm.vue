d85c72b87150c44a0ac03385709a8e271f7065f0
Whatdoyumin <lym4963@naver.com>
2025-07-26 01:37:30 +0900

    🐛 fix: 잘못된 변수명 및 스타일 수정

--- src/components/pre-contract/owner/step4/WolseForm.vue
+++ src/components/pre-contract/owner/step4/WolseForm.vue
@@ -12,7 +12,7 @@
     <div v-if="form.rentAdjustable" class="w-full flex flex-col gap-2">
       <p class="text-sm font-medium text-gray-600">월세 조정 시 하한선을 입력하세요.</p>
       <div class="flex gap-4">
-        <BaseInput label="" v-model="form.rentMin" type="number" placeholder="" class="w-32" />
+        <BaseInput v-model="form.rentMin" type="number" placeholder="" class="w-32" />
         <p class="flex justify-center items-end mb-2 text-sm text-gray-600">만 원</p>
       </div>
     </div>
@@ -29,14 +29,21 @@
     <div v-if="form.depositAdjustable" class="w-full flex flex-col gap-2">
       <p class="text-sm font-medium text-gray-600">보증금 조정 시 하한선을 입력하세요.</p>
       <div class="flex gap-4">
-        <BaseInput label="" type="number" placeholder="" class="w-32" />
+        <BaseInput v-model="form.depositMin" type="number" placeholder="" class="w-32" />
         <p class="flex justify-center items-end mb-2 text-sm text-gray-600">만 원</p>
       </div>
     </div>
 
     <BaseInput v-model="form.rentDate" label="월세 납부일" type="date" />
 
-    <BaseInput v-model="form.penaltyRate" type="number" placeholder="연체 시 이자율 (%)" />
+    <div class="w-full flex flex-col gap-2">
+      <p class="text-sm font-medium text-gray-600">연체 시 이자율 (%) 입력하세요.</p>
+      <div class="w-40 flex gap-4">
+        <p class="flex justify-center items-end mb-2 text-sm text-gray-600">월</p>
+        <BaseInput v-model="form.penaltyRate" type="number" placeholder="" class="w-32" />
+        <p class="flex justify-center items-end mb-2 text-sm text-gray-600">%</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -46,14 +53,22 @@ import ToggleRadio from '@/components/common/ToggleRadio.vue'
 import BaseInput from '@/components/common/BaseInput.vue'
 
 const model = defineModel()
+
 const form = reactive({
-  rentAdjustable: '',
-  rentMin: '',
-  depositAdjustable: '',
-  depositMin: '',
+  rentAdjustable: false,
+  rentMin: 0,
+  depositAdjustable: false,
+  depositMin: 0,
   rentDate: '',
-  penaltyRate: '',
+  penaltyRate: 0,
 })
 
-watch(form, () => (model.value = form), { deep: true })
+// form이 변경될 때 부모에게 반영
+watch(
+  form,
+  () => {
+    model.value = { ...form }
+  },
+  { deep: true },
+)
 </script>
