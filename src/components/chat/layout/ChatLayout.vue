7a88df3010e899c9e59a2820e338ff434d63f738
leeedongjaee <harold306122@gmail.com>
2025-07-25 15:23:20 +0900

    ✨ feat:레이아웃 수정

--- src/components/chat/layout/ChatLayout.vue
+++ src/components/chat/layout/ChatLayout.vue
@@ -5,7 +5,7 @@
 
     <!-- 메인 채팅 영역 -->
     <div class="flex-1">
-      <slot :selected-room="selectedRoom" />
+      <slot :selectedRoom="selectedRoom" />
     </div>
   </div>
 </template>
@@ -17,7 +17,7 @@ import { ref } from 'vue'
 const selectedRoom = ref(null)
 
 function selectRoom(room) {
-  if (selectedRoom.value?.id === room.id) {
+  if (selectedRoom.value?.chatRoomId === room.chatRoomId) {
     selectedRoom.value = null // 같은 방을 클릭하면 해제
   } else {
     selectedRoom.value = room
