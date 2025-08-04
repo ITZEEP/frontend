5e21ae6e6810350c571668447643e86cf5e240a8
Whatdoyumin <lym4963@naver.com>
2025-08-05 04:49:28 +0900

    ✨ feat: 모달 store를 컴포넌트별 실행 가능하도록 수정

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -3,17 +3,25 @@ import { defineStore } from 'pinia'
 
 export const useModalStore = defineStore('modal', () => {
   const isOpen = ref(false)
+  const currentModalComponent = ref(null)
+  const modalProps = ref({})
 
-  function open() {
+  function open(component, props = {}) {
+    currentModalComponent.value = component
+    modalProps.value = props
     isOpen.value = true
   }
 
   function close() {
     isOpen.value = false
+    currentModalComponent.value = null
+    modalProps.value = {}
   }
 
   return {
     isOpen,
+    currentModalComponent,
+    modalProps,
     open,
     close,
   }
