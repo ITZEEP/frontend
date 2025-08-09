bf72389fad5589eac3a6a6ae17dca032722c4709
MeongW <125392666+MeongW@users.noreply.github.com>
2025-08-09 16:13:42 +0900

    :sparkles:feat: 위험도 체크 주소 입력 개선 및 홈 페이지 구현 (#98)
    * fix: 채팅 API 개선 및 타임아웃 설정 조정
    
    * feat: 채팅방 자동 선택 및 이동 기능 구현
    
    * feat: 위험도 체크 결과에서 채팅방 생성 기능 추가
    
    * fix: 마이페이지 거래 정보 표시 및 입력 폼 개선
    
    * :sparkles: feat: 홈
    
    * :bug: fix: 매물 주소 입력 수정

--- src/components/chat/layout/ChatLayout.vue
+++ src/components/chat/layout/ChatLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-full">
     <div class="w-1/3 border-r">
-      <ChatList @selectRoom="handleRoomSelection" ref="chatListRef" />
+      <ChatList @selectRoom="handleRoomSelection" :initial-room-id="initialRoomId" ref="chatListRef" />
     </div>
     <div class="flex-1">
       <!--  선택된 채팅방이 있을 때만 렌더링 -->
@@ -132,10 +132,17 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import ChatList from '@/components/chat/chatList/ChatList.vue'
 import ChatRoom from '@/components/chat/chatRoom/ChatRoom.vue'
 import IconChatWait from '@/components/icons/IconChatWait.vue'
+
+const props = defineProps({
+  initialRoomId: {
+    type: String,
+    default: null,
+  },
+})
 
 const selectedRoom = ref(null) //  초기값을 null로 설정
 const chatListRef = ref(null)
@@ -160,4 +167,3 @@ function handleRoomClosed() {
     chatListRef.value.setCurrentChatRoom(null)
   }
 }
-</script>
