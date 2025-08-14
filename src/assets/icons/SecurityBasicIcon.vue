c442eb3abc821300467a4356f48a18085346f25a
Whatdoyumin <lym4963@naver.com>
2025-08-14 17:18:25 +0900

    💄 style: 크기 수정

--- src/assets/icons/SecurityBasicIcon.vue
+++ src/assets/icons/SecurityBasicIcon.vue
@@ -2,9 +2,10 @@
   <svg
     :width="width"
     :height="height"
-    viewBox="0 0 24 24"
+    viewBox="0 0 13.2 14.4"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid meet"
   >
     <path
       fill="currentColor"
